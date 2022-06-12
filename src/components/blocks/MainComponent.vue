ec5efaad43089ae663502fc788880555b2fdb209
Onda-dev <manuelaltieri3@gmail.com>
2022-06-12 19:11:28 +0200

    Milestone 1 ok

--- src/components/blocks/MainComponent.vue
+++ src/components/blocks/MainComponent.vue
@@ -1,12 +1,28 @@
 <template>
   <main>
-
+      <div class="container">
+        <ul>
+            <li v-for="movie in data.movies" :key="movie.id">
+                <h3 class="title">{{movie.title}}</h3>
+                <h4 class="original-title">{{movie.original_title}}</h4>
+                <div class="vote">{{movie.vote_average}}</div>
+                <div class="language">{{movie.original_title}}</div>
+            </li>
+        </ul>
+      </div>
   </main>
 </template>
 
 <script>
+import data from '../../shared/data'
+
 export default {
-    name: 'MainComponent'
+    name: 'MainComponent',
+    data(){
+        return {
+            data
+        }
+    }
 }
 </script>
 
