78058a94394d5163eee4f29347fce0b48cd326ec
Onda-dev <manuelaltieri3@gmail.com>
2022-06-13 17:27:35 +0200

    poster ok

--- src/components/commons/CardProduct.vue
+++ src/components/commons/CardProduct.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="card">
+        <img :src="getUrl(info.poster_path)" alt="poster">
         <h3 class="title">{{info.title ? info.title : info.name}}</h3>
         <h4 class="original-title">{{info.original_title ? info.original_title : info.original_name}}</h4>
         <div class="vote">{{info.vote_average}}</div>
@@ -12,6 +13,11 @@ export default {
     name: 'CardProduct',
     props: {
         info: Object
+    },
+    methods: {
+        getUrl(url) {
+            return `http://image.tmdb.org/t/p/w185/${url}`
+        }
     }
 }
 </script>
